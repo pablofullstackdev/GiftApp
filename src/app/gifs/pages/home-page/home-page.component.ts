64807f4c4cca0b463cdf4d56d809f2a841d01b41
Pablo <pablofullstack@protonmail.com>
2024-05-14 21:08:46 +0200

    card-list component and @ViewChild

--- src/app/gifs/pages/home-page/home-page.component.ts
+++ src/app/gifs/pages/home-page/home-page.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css',
 })
 export class HomePageComponent {}
